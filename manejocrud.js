d43462e63a714d10f334242e8af657631288a498
Mary <maribel.ramirez04@inacapmail.cl>
2022-06-07 19:26:23 -0400

    E-Commerce_Modificado
    Ordenado

--- manejocrud.js
+++ manejocrud.js
@@ -1,7 +1,7 @@
 // Orden Matriz: Codigo, Nombre, Precio, Descripción, Stok
 
-var matriz=[
-	["Cupon1","Plupones Manolino", 22900,"Plumón Coral Fleece estampado en modelo y tamaño a elección", 30],
+var matriz = [
+	["Cupon1", "Plupones Manolino", 22900, "Plumón Coral Fleece estampado en modelo y tamaño a elección", 30],
 	//["Cupon2","V for Vegan", 12990, "Entrada para compartir + playos de fondo + postres + bebestibles", 10],
 	//["Cupon3","Clinica Dental",259900, "Evaluación integral + blanqueamiento dental led + limpieza + fluoración", 5],
 	//["Cupon4", "Suricata Almacen", 5100, "Colación para 2 personas + postre", 20],
@@ -11,87 +11,87 @@ var matriz=[
 
 
 
-function ingresarCupon(){
-	var codigo=document.getElementById("codigo").value;
-	var nombre=document.getElementById("nombre").value;
-	var precio=document.getElementById("precio").value;
-	var descripcion=document.getElementById("descripcion").value;
-	var stok=document.getElementById("stok").values;
-	matriz.push([codigo,nombre,precio,descripcion,stok]);
+function ingresarCupon() {
+	var codigo = document.getElementById("codigo").value;
+	var nombre = document.getElementById("nombre").value;
+	var precio = document.getElementById("precio").value;
+	var descripcion = document.getElementById("descripcion").value;
+	var stok = document.getElementById("stok").values;
+	matriz.push([codigo, nombre, precio, descripcion, stok]);
 	alert("Cupon Insertado");
-	document.getElementById("codigo").value="";
-	document.getElementById("nombre").value="";
-	document.getElementById("precio").value="";
-	document.getElementById("descripcion").value="";
-	document.getElementById("stok").value="";
+	document.getElementById("codigo").value = "";
+	document.getElementById("nombre").value = "";
+	document.getElementById("precio").value = "";
+	document.getElementById("descripcion").value = "";
+	document.getElementById("stok").value = "";
 
 }
 
-function mostrarCupon(){
-	var largo=matriz.length;
-	
-	for(var x=0; x<largo;x++){
-		for(var y=0; y<matriz[x].length;y++){
+function mostrarCupon() {
+	var largo = matriz.length;
+
+	for (var x = 0; x < largo; x++) {
+		for (var y = 0; y < matriz[x].length; y++) {
 			alert(matriz[x][y]);
-			document.getElementById("codigo").value="";
+			document.getElementById("codigo").value = "";
 		}
 	}
 }
 
-function actualizarCupon(){
+function actualizarCupon() {
 	//actualización de precio por medio del codigo
-	var codigo=document.getElementById("codigo").value;
-	var precio=document.getElementById("precio").value;
-	var largo=matriz.length;
-	for(var x=0; x<largo;x++){
-		for(var y=0; y<matriz[x].length;y++){
-			
-			if(matriz[x][y]==codigo){
-				matriz[x][y+1]=[precio];
+	var codigo = document.getElementById("codigo").value;
+	var precio = document.getElementById("precio").value;
+	var largo = matriz.length;
+	for (var x = 0; x < largo; x++) {
+		for (var y = 0; y < matriz[x].length; y++) {
+
+			if (matriz[x][y] == codigo) {
+				matriz[x][y + 1] = [precio];
 				alert("Cupon Actualizado");
 			}
 
 		}
 	}
-	document.getElementById("codigo").value="";
-	document.getElementById("precio").value="";
+	document.getElementById("codigo").value = "";
+	document.getElementById("precio").value = "";
 }
 
-function actualizarStokCupon(){
+function actualizarStokCupon() {
 	//actualización de Stok por medio del codigo
-	var codigo=document.getElementById("codigo").value;
-	var stok=document.getElementById("stok").value;
-	
-	var largo=matriz.length;
-	for(var x=0; x<largo;x++){
-		for(var y=0; y<matriz[x].length;y++){
-			
-			if(matriz[x][y]==codigo){
-				matriz[x][y+1]=[stok];
+	var codigo = document.getElementById("codigo").value;
+	var stok = document.getElementById("stok").value;
+
+	var largo = matriz.length;
+	for (var x = 0; x < largo; x++) {
+		for (var y = 0; y < matriz[x].length; y++) {
+
+			if (matriz[x][y] == codigo) {
+				matriz[x][y + 1] = [stok];
 				alert("Stok Cupon Actualizado");
 			}
 
 		}
 	}
-	document.getElementById("codigo").value="";
-	document.getElementById("stok").value="";
+	document.getElementById("codigo").value = "";
+	document.getElementById("stok").value = "";
 }
 
-function eliminarCupon(){
-//eliminar por codigo
-	var codigo=document.getElementById("codigo").value;
-	
-	var largo=matriz.length;
-	for(var x=0; x<largo;x++){
-		for(var y=0; y<matriz[x].length;y++){
-			
-			if(matriz[x][y]==codigo){
-				matriz.splice([x][y],1);
+function eliminarCupon() {
+	//eliminar por codigo
+	var codigo = document.getElementById("codigo").value;
+
+	var largo = matriz.length;
+	for (var x = 0; x < largo; x++) {
+		for (var y = 0; y < matriz[x].length; y++) {
+
+			if (matriz[x][y] == codigo) {
+				matriz.splice([x][y], 1);
 				alert("Cupon Eliminado");
-				document.getElementById("codigo").value="";
+				document.getElementById("codigo").value = "";
 			}
 
 		}
 	}
-	
+
 }
